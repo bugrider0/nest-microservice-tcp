facc4ea687e46e89aa1baab1cf1b4e82c5bd4149
hsn0najafi <hsn0najafi@gmail.com>
2022-06-16 14:23:46 +0430

    feat(microservice): swagger setup

--- microservice/src/products/dto/update-product.dto.ts
+++ microservice/src/products/dto/update-product.dto.ts
@@ -1,4 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
